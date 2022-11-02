fcd54c8345b07de9334100a1f0506f4ddd856f53
akrivosheevgowi <105637402+akrivosheevgowi@users.noreply.github.com>
2022-11-02 21:41:41 +0100

    1480. Running Sum of 1d Array

---
+++ leetcode/1480_Running_Sum_1d_Array/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode._1480_Running_Sum_1d_Array
+{
+    public static class Solution
+    {
+        public static int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+
+            return nums;
+        }
+    }
+}
---
+++ leetcode/1480_Running_Sum_1d_Array/Tests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace leetcode._1480_Running_Sum_1d_Array
+{
+    public static class Tests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4 }, new int[] { 1, 3, 6, 10 })]
+        [InlineData(new int[] { 1,1,1,1 }, new int[] { 1, 2, 3, 4 })]
+        public static void RunningSum_Case_1(int[] numbers, int[] expected)
+        {
+            var result = Solution.RunningSum(numbers);
+            
+            result.Should().Equal(expected);            
+        }      
+    }
+}
