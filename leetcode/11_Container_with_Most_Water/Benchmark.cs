b6ece4c661207dbc567d7478423a8eff14043e8c
akrivosheevgowi <105637402+akrivosheevgowi@users.noreply.github.com>
2023-02-01 22:07:05 +0100

    Added 11 tasks

---
+++ leetcode/11_Container_with_Most_Water/Benchmark.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode._11_Container_with_Most_Water
+{
+    [MemoryDiagnoser]        
+    public class Benchmark
+    {
+        [Benchmark]
+        public void Solution1()
+        {
+            
+        }
+    }
+}
---
+++ leetcode/11_Container_with_Most_Water/Solution1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode._11_Container_with_Most_Water
+{
+    public class Solution1
+    {
+        public int MaxArea(int[] height)
+        {
+            int literOfWater = 0;
+            int start = 0;
+            int end = height.Length - 1;
+            int maxLiterOfWater = 0;
+
+            do
+            {
+                literOfWater = Math.Min(height[start], height[end]) * (end - start);
+                if (maxLiterOfWater < literOfWater)
+                {
+                    maxLiterOfWater = literOfWater;
+                }
+
+                if (height[start] < height[end])
+                {
+                    start++;
+                }
+                else
+                {
+                    end--;
+                }
+
+            } while (start < end);
+
+            return maxLiterOfWater;
+        }
+    }
+}
