949e722119b338e2362302ce02f8bf8fc39694af
alexzoa <124262010+alexzoa@users.noreply.github.com>
2024-03-03 22:54:55 +0100

    Added 226

---
+++ leetcode/226_invert_binary_tree/Solution1.cs
@@ -0,0 +1,43 @@
+﻿namespace leetcode._invert_binary_tree
+{
+    public class Solution
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+
+            return InvertNode(root);
+        }
+
+        public TreeNode InvertNode(TreeNode root)
+        {
+
+            if (root == null)
+            {
+                return null;
+            }
+
+            var left = root.left;
+            var right = root.right;
+
+            if (right != null)
+            {
+                root.left = InvertNode(right);
+            }
+            else
+            {
+                root.left = null;
+            }
+
+            if (left != null)
+            {
+                root.right = InvertNode(left);
+            }
+            else
+            {
+                root.right = null;
+            }
+
+            return root;
+        }
+    }
+}
