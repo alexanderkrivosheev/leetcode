c4be5070925fd553a4697bd21b114c879eb8d064
alexzoa <124262010+alexzoa@users.noreply.github.com>
2024-03-04 14:25:30 +0100

    added 100

---
+++ leetcode/100_same_tree/Solution1.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode._same_tree
+{
+    public class Solution
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if ((p == null && q != null) || (q == null && p != null))
+            {
+                return false;
+            }
+
+            if (p != null && q != null)
+            {
+                if (p.val != q.val)
+                {
+                    return false;
+                }
+
+                return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ leetcode/100_same_tree/Tests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace leetcode._same_tree
+{
+    public static class Tests
+    {
+        [Theory]
+
+
+        [InlineData(1)]
+        public static void Solution_Case_1(int l)
+        {
+            TreeNode head = new TreeNode() { val = 1 };
+            head.left = new TreeNode() { val = 2 };
+            head.right = new TreeNode() { val = 3 };
+
+            TreeNode head2 = new TreeNode() { val = 1 };
+            head2.left = new TreeNode() { val = 2 };
+            head2.right = new TreeNode() { val = 3 };
+
+            var solution = new Solution();
+            solution.IsSameTree(head, head2);
+        }
+    }
+}
+  
