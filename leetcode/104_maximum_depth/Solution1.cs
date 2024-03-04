f68f23a726d6a9e340a3797100d7ca7892b26f87
alexzoa <124262010+alexzoa@users.noreply.github.com>
2024-03-04 09:02:02 +0100

    added 104 task

---
+++ leetcode/104_maximum_depth/Solution1.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode._maximum_depth
+{
+    public class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            if (root.left == null && root.right == null)
+            {
+                return 1;
+            }
+
+            if (root.left == null && root.right != null)
+            {
+                return MaxDepth(root.right) + 1;
+            }
+
+            if (root.right == null && root.left != null)
+            {
+                return MaxDepth(root.left) + 1;
+            }
+
+            var lefDepth = MaxDepth(root.left);
+            var rightDepth = MaxDepth(root.right);
+
+            return (lefDepth > rightDepth ? lefDepth : rightDepth) + 1;
+        }
+    }
+}
