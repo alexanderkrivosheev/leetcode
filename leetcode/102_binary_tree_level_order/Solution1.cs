1db5657296ae9bb001b5b702ee502f1f63783811
alexzoa <124262010+alexzoa@users.noreply.github.com>
2024-03-13 10:40:19 +0100

    Added 102 Task

---
+++ leetcode/102_binary_tree_level_order/Solution1.cs
@@ -0,0 +1,52 @@
+﻿namespace leetcode._binary_tree_level_order
+{
+    public class Solution
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+
+            var result = new List<IList<int>>();
+
+            if (root == null)
+            {
+                return result;
+            }
+
+            var q = new Queue<TreeNode>();
+            q.Enqueue(root);
+
+            while (true)
+            {
+                var curLevelCount = q.Count;
+                if (curLevelCount == 0)
+                {
+                    break;
+                }
+
+                var curNodes = new List<int>();
+
+                while (curLevelCount > 0)
+                {
+                    var cur = q.Dequeue();
+                    curNodes.Add(cur.val);
+
+                    if (cur.left != null)
+                    {
+                        q.Enqueue(cur.left);
+                    }
+
+                    if (cur.right != null)
+                    {
+                        q.Enqueue(cur.right);
+                    }
+
+                    curLevelCount--;
+                }
+
+                result.Add(curNodes);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ leetcode/102_binary_tree_level_order/Tests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace leetcode._binary_tree_level_order
+{
+    public static class Tests
+    {
+        [Theory]
+
+
+        [InlineData(1)]
+        public static void Solution_Case_1(int l)
+        {
+            TreeNode head = new TreeNode() { val = 1 };
+            var node = head;
+
+            head.left = new TreeNode() { val = 2 };
+            head.right = new TreeNode() { val = 3 };
+
+            var solution = new Solution();
+            solution.LevelOrder(head);
+        }
+    }
+}
+  
