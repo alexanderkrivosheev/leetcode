422e669c9389479389d3297d61b5df051d44b4c7
alexzoa <124262010+alexzoa@users.noreply.github.com>
2024-03-25 09:24:28 +0100

    Added 230 tasks

---
+++ leetcode/230_smallest_element/Solution1.cs
@@ -0,0 +1,43 @@
+﻿using Iced.Intel;
+using leetcode._205_reverse_linked_list;
+using System.Configuration;
+
+namespace leetcode._smallest_element
+{
+    public class Solution1
+    {
+        public int KthSmallest(TreeNode root, int k)
+        {
+            return HandleNode(root, ref k).Value;
+        }
+
+        private int? HandleNode(TreeNode root, ref int k)
+        {
+            if (root.left != null)
+            {
+                var res = HandleNode(root.left, ref k);
+                if (res.HasValue)
+                {
+                    return res;
+                }
+            }
+
+            k--;
+            if (k <= 0)
+            {
+                return root.val;
+            }
+
+            if (root.right != null)
+            {
+                var res = HandleNode(root.right, ref k);
+                if (res.HasValue)
+                {
+                    return res;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ leetcode/230_smallest_element/Tests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace leetcode._smallest_element
+{
+    public static class Tests
+    {
+        [Theory]
+
+        [InlineData(1)]
+        public static void Solution_Case_1(int l)
+        {
+            TreeNode head = new TreeNode() { val = 1 };
+            var node = head;
+
+            head.left = new TreeNode() { val = 2 };
+            head.right = new TreeNode() { val = 3 };
+           
+            var solution = new Solution1();
+            solution.KthSmallest(head,1);
+            
+        }
+    }
+}
+  
