94a7e0bc4b2102bd41699ec3d3536d517320eda2
alexzoa <124262010+alexzoa@users.noreply.github.com>
2024-04-05 17:15:12 +0200

    Added 105 task

---
+++ leetcode/Trees/105_preorder-and-inorder/Solution1.cs
@@ -0,0 +1,36 @@
+﻿namespace leetcode._preorder_and_inorder
+{
+    public class Solution
+    {
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            return BuildTreeImpl(preorder, inorder, 0, 0, preorder.Length);
+        }
+
+        public TreeNode BuildTreeImpl(int[] preorder, int[] inorder, int preIndex = 0, int inL = 0, int inR = 0)
+        {
+            if (preorder.Length == 0 || inorder.Length == 0)
+            {
+                return null;
+            }
+
+            if (preIndex > preorder.Length - 1 || inL > inR)
+            {
+                return null;
+            }
+
+            var newNode = new TreeNode(val: preorder[preIndex]);
+
+            int mid = inL;
+            while (inorder[mid] != newNode.val)
+            {
+                mid++;
+            }
+
+            newNode.left = BuildTreeImpl(preorder, inorder, preIndex + 1, inL, mid - 1);
+            newNode.right = BuildTreeImpl(preorder, inorder, preIndex + mid - inL + 1, mid + 1, inR);
+
+            return newNode;
+        }
+    }
+}
---
+++ leetcode/Trees/105_preorder-and-inorder/Tests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace leetcode._preorder_and_inorder
+{
+    public static class Tests
+    {
+        [Theory]
+
+        [InlineData(new int[] { 3, 9, 20, 15, 7 }, new int[] { 9, 3, 15, 20, 7 })]
+        public static void Solution_Case_1(int[] preorder, int[] inorder)
+        {
+            var solution = new Solution();
+            solution.BuildTree(preorder, inorder);
+        }
+    }
+}
+  
