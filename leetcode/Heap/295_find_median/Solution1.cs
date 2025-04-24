156f798cc2bb16619028ff01cd9522bd755e7be8
Alexander Krivosheev <alexander.krivosheev@hotmail.com>
2025-04-24 13:50:01 -0400

    Added 295 Task

---
+++ leetcode/Heap/295_find_median/Solution1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Heap
+{
+    public class MedianFinder        
+    {
+        private PriorityQueue<int, int> SmallHeap = new PriorityQueue<int, int>();
+        private PriorityQueue<int, int> LargeHeap = new PriorityQueue<int, int>();
+
+        public MedianFinder()
+        {
+            
+        }
+
+        
+        public void AddNum(int num)
+        {
+            int? maxSmallHeap = SmallHeap.Count>0? SmallHeap.Peek(): null;
+            int? maxLargeHeap = LargeHeap.Count>0? LargeHeap.Peek():null;
+
+
+            if(maxLargeHeap.HasValue && num > maxLargeHeap.Value)
+            {
+                LargeHeap.Enqueue(num, num);
+            }
+            else
+            {
+                SmallHeap.Enqueue(num, -num);
+            }
+
+            if (SmallHeap.Count > LargeHeap.Count + 1)
+            {
+                maxSmallHeap = SmallHeap.Dequeue();
+                LargeHeap.Enqueue(maxSmallHeap.Value, maxSmallHeap.Value);
+            }
+
+            if (LargeHeap.Count > SmallHeap.Count + 1)
+            {
+                maxSmallHeap = LargeHeap.Dequeue();
+                SmallHeap.Enqueue(maxSmallHeap.Value, -maxSmallHeap.Value);
+            }
+        }
+
+        public double FindMedian()
+        {
+            if(SmallHeap.Count == LargeHeap.Count)
+            {
+                return (a + b) /2.0;
+            }
+
+            if (SmallHeap.Count > LargeHeap.Count)
+            {
+                return SmallHeap.Peek();
+            }
+
+            if (SmallHeap.Count < LargeHeap.Count)
+            {
+                return LargeHeap.Peek();
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ leetcode/Heap/295_find_median/Tests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace leetcode.Heap
+{
+    public static class Tests
+    {
+        [Fact]
+        public static void Solution_Case_1()
+        {
+
+            MedianFinder finder = new MedianFinder();
+            finder.AddNum(1);    // arr = [1]
+            finder.AddNum(2);    // arr = [1, 2]
+            var result = finder.FindMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+            finder.AddNum(3);    // arr[1, 2, 3]
+            result = finder.FindMedian(); // return 2.0
+
+        }
+    }
+}
+  
